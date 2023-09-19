b818104c1d274131b83ef9911c0f8ac72da9b3f1
machete <rtpppp163@gmail.com>
2023-09-20 01:55:41 +0800

    f

--- script/emby.js
+++ script/emby.js
@@ -1,9 +1,12 @@
 const { url } = $request
 const { body } = $response
 const OK_STATUS = 'HTTP/1.1 200 OK'
-$notify('emby', 'emby', JSON.stringify({ url, body }))
+
+const doneRes = {
+  body,
+}
 if (url.includes('/admin/service/registration/validateDevice')) {
-  $done({
+  Object.assign(doneRes, {
     status: OK_STATUS,
     body: JSON.stringify({
       cacheExpirationDays: 365,
@@ -12,5 +15,6 @@ if (url.includes('/admin/service/registration/validateDevice')) {
     }),
   })
 }
+$notify('emby', 'emby', JSON.stringify(doneRes))
 
-$done({ body })
+$done(doneRes)
