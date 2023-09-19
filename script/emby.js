152ffdb2fde8fc45127b443afe2d963ea4224789
machete <rtpppp163@gmail.com>
2023-09-20 02:11:34 +0800

    f

--- script/emby.js
+++ script/emby.js
@@ -2,19 +2,46 @@ const { url } = $request
 const { body } = $response
 const OK_STATUS = 'HTTP/1.1 200 OK'
 
-const doneRes = {
-  body,
-}
-if (url.includes('/admin/service/registration/validateDevice')) {
-  Object.assign(doneRes, {
-    status: OK_STATUS,
-    body: JSON.stringify({
-      cacheExpirationDays: 365,
-      message: 'Device Valid',
-      resultCode: 'GOOD',
-    }),
-  })
-}
-$notify('emby', 'emby', JSON.stringify(doneRes))
+let modifyBody = JSON.parse(body)
 
-$done(doneRes)
+if (url.includes('/admin/service/registration/validateDevice')) {
+  modifyBody = {
+    cacheExpirationDays: 365,
+    message: 'Device Valid',
+    resultCode: 'GOOD',
+  }
+  /* cspell:disable-next-line */
+} else if (url.includes('/admin/service/appstore/register')) {
+  modifyBody = {
+    featId: '',
+    registered: true,
+    expDate: '2099-01-01',
+    key: '',
+  }
+} else if (url.includes('/admin/service/registration/validate')) {
+  modifyBody = {
+    featId: '',
+    registered: true,
+    expDate: '2099-01-01',
+    key: '',
+  }
+} else if (url.includes('/admin/service/registration/getStatus')) {
+  modifyBody = {
+    planType: 'Cracked',
+    deviceStatus: '',
+    subscriptions: [],
+  }
+  /* cspell:disable-next-line */
+} else if (url.includes('/admin/service/supporter/retrievekey')) {
+  modifyBody = {
+    Success: false,
+    ErrorMessage: 'Supporter not found',
+  }
+}
+
+$notify('emby', 'emby', JSON.stringify(modifyBody))
+
+$done({
+  status: OK_STATUS,
+  body: JSON.stringify(modifyBody),
+})
